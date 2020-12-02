f1c91710e0d91435a8e5a1aa021a4b837c4ca4ce
lorossi <lo.rossi@live.com>
2020-12-02 23:37:33 +0100

    added sub method

--- vectors.js
+++ vectors.js
@@ -13,6 +13,10 @@
 * @param {number} [0] x - The x value
 * @param {number} [0] y - The y value
 * @param {number} [0] z - The z value
+* @example
+* v1 = new Vector(1, 4, -3);
+* @example
+* v2 = new Vector(3, -5);
 */
 function Vector(x, y, z) {
   this.x = x || 0;
@@ -22,8 +26,13 @@ function Vector(x, y, z) {
 
 Vector.prototype = {
   /**
-  * Add a vector together
+  * Add a vector
   * @param {Vector} v - The vector to be added
+  * @example
+  * v1 = new Vector(1, -4, 12);
+  * v2 = new Vector(2, 9, -3);
+  * v1.add(v2);
+  * // v1 = Vector(3, 5, 9);
   */
   add: function(v) {
     if (v instanceof Vector) {
@@ -36,6 +45,11 @@ Vector.prototype = {
   /**
   * Subract a vector
   * @param {Vector} v - The vector to be subracted
+  * @example
+  * v1 = new Vector(10, -3, 12);
+  * v2 = new Vector(7, -8, 3);
+  * v1.sub(v2);
+  * // v1 = Vector(3, 5, 9);
   */
   sub: function(v) {
     if (v instanceof Vector) {
@@ -46,8 +60,25 @@ Vector.prototype = {
   },
 
   /**
+  * Alias for sub
+  * @alias sub
+  */
+  subtract: function(v) {
+    sub(v);
+  },
+
+  /**
   * Multiply by a vector or a scalar
   * @param {Vector|number} v - The vector or scalar to be multiplied by
+  * @example
+  * v1 = new Vector(1, 2, 3);
+  * v2 = new Vector(2, 5, 0);
+  * v1.mult(v2);
+  * // v1 = Vector(2, 10, 0);
+  * @example
+  * v1 = new Vector(7, 4, 2);
+  * v1.mult(3);
+  * // v1 = Vector(21, 12, 6);
   */
   multiply: function(v) {
     if (v instanceof Vector) {
@@ -64,6 +95,14 @@ Vector.prototype = {
   /**
   * Divide by a vector or a scalar
   * @param {Vector|number} v - The vector or scalar to be divided by
+  * v1 = new Vector(4, 12, 9);
+  * v2 = new Vector(4, 6, 3);
+  * v1.divide(v2);
+  * // v1 = Vector(1, 2, 3);
+  * @example
+  * v1 = new Vector(9, 3, 6);
+  * v1.divide(3);
+  * // v1 = Vector(3, 1, 2);
   */
   divide: function(v) {
     if (v instanceof Vector) {
@@ -78,8 +117,12 @@ Vector.prototype = {
   },
 
   /**
-  * Multiply by a or a scalar
+  * Multiply by a  scalar
   * @param {number} s - The scalar to be multiplied by
+  * @example
+  * v1 = new Vector(7, 4, 2);
+  * v1.multiply_scalar(3);
+  * // v1 = Vector(21, 12, 6);
   */
   multiply_scalar: function(s) {
     this.multiply(s);
@@ -88,6 +131,10 @@ Vector.prototype = {
   /**
   * Divide by a scalar
   * @param {number} s - The scalar to be divided by
+  * @example
+  * v1 = new Vector(9, 3, 6);
+  * v1.divide_scalar(3);
+  * // v1 = Vector(3, 1, 2);
   */
   divide_scalar: function(s) {
     this.divide(s);
@@ -96,6 +143,11 @@ Vector.prototype = {
   /**
   * Dot function
   * @param {Vector} v - The vector to perform dot operation with
+  * @example
+  * v1 = new Vector(1, 4, 3);
+  * v2 = new Vector(2, -6, 9);
+  * v1.dot(v2);
+  * // return 5;
   */
   dot: function(v) {
     if (v instanceof Vector) {
@@ -108,6 +160,11 @@ Vector.prototype = {
   /**
   * Cross function
   * @param {Vector} v - The vector to perform cross operation with
+  * @example
+  * v1 = new Vector(1, 4, 3);
+  * v2 = new Vector(2, -6, 9);
+  * v1.cross(v2);
+  * // v1 = Vector(54, -3, -14);
   */
   cross: function(v) {
     if (v instanceof Vector) {
@@ -120,7 +177,12 @@ Vector.prototype = {
   /**
   * Distance between vectors
   * @param {Vector} v - The vector whose distance will be calculated
-  * @returns {Vector} - Returns a vector containing the distance
+  * @return {Vector} Return a vector containing the distance
+  * @example
+  * v1 = new Vector(1, 4, -3);
+  * v2 = new Vector(6, -6, 7);
+  * v1.dist(v2);
+  * // v1 = Vector(-5, 10, -10);
   */
   dist: function(v) {
     if (v instanceof Vector) {
@@ -131,7 +193,12 @@ Vector.prototype = {
   /**
   * Angle between vectors
   * @param {Vector} v - The vector whose contained angle will be calcolated
-  * @returns {number} - Returns a vector containing the angle in radians
+  * @return {number} Return a vector containing the angle in radians
+  * @example
+  * v1 = new Vector(1, 4, -3);
+  * v2 = new Vector(6, -6, 7);
+  * v1.dist(v2);
+  * // v1 = Vector(-5, 10, -10);
   */
   angleBetween: function(v) {
     if (v instanceof Vector) {
@@ -142,7 +209,12 @@ Vector.prototype = {
   /**
   * Check if two vectors are equals
   * @param {Vector} v - The vector that will be compared
-  * @returns {boolean} - Returns true or false
+  * @return {boolean} Return true or false
+  * @example
+  * v1 = new Vector(1, 4, -3);
+  * v2 = new Vector(6, -6, 7);
+  * v1.equal(v2);
+  * // return false;
   */
   equals: function(v) {
     if (v instanceof Vector) {
@@ -152,7 +224,11 @@ Vector.prototype = {
 
   /**
   * Copy the vector into a new objecy
-  * @returns {Vector} - The new copied vector
+  * @return {Vector} The new copied vector
+  * @example
+  * v1 = new Vector(8, 144, -32);
+  * v2 = v1.copy();
+  * // v2 = Vector(8, 144, -32);
   */
   copy: function() {
     return new Vector(this.x, this.y, this.z);
@@ -161,6 +237,10 @@ Vector.prototype = {
   /**
   * Limit the vector magnitude to a set value
   * @param {number} s - The maximum magninute
+  * @example
+  * v1 = new Vector(2, 0, 2);
+  * v1.limit(2);
+  * // v1 = Vector(1.414213562373095, 0, 1.414213562373095);
   */
   limit: function(s) {
     if (typeof(s) === "number") {
@@ -172,8 +252,12 @@ Vector.prototype = {
   },
 
   /**
-  * Set the vector magninute
-  * @param {number} s - Magninute
+  * Set the vector magnitude
+  * @param {number} s - Magnitude
+  * @example
+  * v1 = new Vector(2, 0, 2);
+  * v1.setMag(4);
+  * // v1 = Vector(2.82842712474619, 0, 2.82842712474619);
   */
   setMag: function(s) {
     if (typeof(s) === "number") {
@@ -185,6 +269,10 @@ Vector.prototype = {
   /**
   * Rotate a vector by an angle in randians
   * @param {number} t - The rotation angle
+  * @example
+  * v1 = new Vector(2, 1);
+  * v1.rotate(Math.PI);
+  * // v1 = Vector(-2, -1, 0);
   */
   rotate: function(t) {
     if (typeof(t) === "number") {
@@ -195,7 +283,13 @@ Vector.prototype = {
     }
   },
 
-  /* Normalize a vector (its magnitude will be unitary) */
+  /**
+  * Normalize a vector (its magnitude will be unitary)
+  * @example
+  * v1 = new Vector(5, 2, -4);
+  * v1.normalize();
+  * // v1 = Vector(0.7453559924999299, 0.29814239699997197, -0.5962847939999439);
+  */
   normalize: function() {
     this.divide_scalar(this.mag());
   },
@@ -205,6 +299,14 @@ Vector.prototype = {
   * @param {boolean} x - The x component
   * @param {boolean} y - The y component
   * @param {boolean} z - The z component
+  * @example
+  * v1 = new Vector(4, -5, 7);
+  * v1.invert(true, true, true);
+  * // v1 = Vector(-4, 5, -7);
+  * @example
+  * v2 = new Vector(4, -1, -3);
+  * v2.invert(true, false);
+  * // v2 = Vector(-4, -1, -3);
   */
   invert: function(x, y, z) {
     if (x) {
@@ -224,24 +326,46 @@ Vector.prototype = {
     }
   },
 
-  /* Invert the x component of the vector */
+  /**
+  * Invert the x component of the vector
+  * @example
+  * v1 = new Vector(4, -5, 7);
+  * v1.invertX();
+  * // v1 = Vector(-4, -5, 7);
+  */
   invertX: function() {
     this.invert(true, false, false);
   },
 
-  /* Invert the y component of the vector */
+  /**
+  * Invert the y component of the vector
+  * @example
+  * v1 = new Vector(4, -5, 7);
+  * v1.invertY();
+  * // v1 = Vector(4, 5, 7);
+  */
   invertY: function() {
     this.invert(false, true, false);
   },
 
-  /* Invert the z component of the vector */
+  /**
+  * Invert the z component of the vector
+  * @example
+  * v1 = new Vector(4, -5, 7);
+  * v1.invertZ();
+  * // v1 = Vector(4, -5, -7);
+  */
   invertZ: function() {
     this.invert(false, false, true);
   },
 
   /**
   * Calculate the vector magnitude
-  * @returns {number} - The vector magnitude
+  * @return {number} - The vector magnitude
+  * @example
+  * v1 = new Vector(6, -2, -1);
+  * v1.mag();
+  * // return 6.4031242374328485;
   */
   mag: function() {
     return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
@@ -249,15 +373,23 @@ Vector.prototype = {
 
   /**
   * Calculate the vector square magnitude
-  * @returns {number} - The vector square magnitude
+  * @return {number} The vector square magnitude
+  * @example
+  * v1 = new Vector(6, -2, -1);
+  * v1.magSq();
+  * // return 41;
   */
   magSq: function() {
     return this.x * this.x + this.y * this.y + this.z * this.z;
   },
 
   /**
-  * Calculate the vector heading (radians)
-  * @returns {number} - The vector heading (radians)
+  * Calculate the vector heading (radians) - only for 2D vectors
+  * @return {number} The vector heading (radians)
+  * @example
+  * v1 = new Vector(3, 3);
+  * v1.heading2D();
+  * // return 0.7853981633974483
   */
   heading2D: function() {
     return Math.atan2(this.y, this.x);
@@ -265,7 +397,11 @@ Vector.prototype = {
 
   /**
   * Return a printable string of the vector
-  * @returns {string} - Printable string
+  * @return {string} Printable string
+  * @example
+  * v1 = new Vector(3, 3, -4);
+  * v1.toString();
+  * // return "x: 3, y: 3, z: -4"
   */
   toString: function() {
     return `x: ${this.x}, y: ${this.y}, z: ${this.z}`;
@@ -274,8 +410,11 @@ Vector.prototype = {
 
 /**
 * Create a 2D vector from its angle
-* @param {number} [0] theta - Theta angle
+* @param {number} [0] theta - Theta angle (radians)
 * @return {Vector} - The new vector
+* @example
+* v1 = new Vector.fromAngle2D(2.42);
+* // v1 = Vector(-0.7507546047254909,0.6605812012792007, 0);
 */
 Vector.fromAngle2D = function(theta) {
   theta = theta || 0;
@@ -284,9 +423,12 @@ Vector.fromAngle2D = function(theta) {
 
 /**
 * Create a 3D vector from its angles
-* @param {number} [0] theta - Theta angle
-* @param {number} [0] phi - Phi angle
+* @param {number} [0] theta - Theta angle (radians)
+* @param {number} [0] phi - Phi angle (radians)
 * @return {Vector} - The new vector
+* @example
+* v1 = new Vector.fromAngle2D(1.33, -2.44);
+* // v1 = Vector(-0.1821516349441893, -0.6454349983343708, -0.7417778945292652);
 */
 Vector.fromAngle3D = function(theta, phi) {
   theta = theta || 0;
@@ -297,6 +439,9 @@ Vector.fromAngle3D = function(theta, phi) {
 /**
 * Create a random 2D vector
 * @return {Vector} - The new vector
+* @example
+* v1 = new Vector.random2D();
+* // v1 = Vector(0.2090564102081952, -0.977903582849998, 0);
 */
 Vector.random2D = function() {
   let theta = Math.random() * 2 * Math.PI;
@@ -306,6 +451,9 @@ Vector.random2D = function() {
 /**
 * Create a random 3D vector
 * @return {Vector} - The new vector
+* @example
+* v1 = new Vector.random3D();
+* // v1 = Vector(-0.7651693875628326, -0.43066633476756877, 0.47858365667309205);
 */
 Vector.random3D = function() {
   let theta = Math.random() * 2 * Math.PI;
@@ -316,10 +464,10 @@ Vector.random3D = function() {
 /**
 * Create a vector from an Array
 * @example
-* // returns Vector(4, 5, 6)
+* // return Vector(4, 5, 6)
 * v = new Vector.fromArray([4, 5, 6])
 * @example
-* // returns Vector(1, 7, 0)
+* // return Vector(1, 7, 0)
 * v = new Vector.fromArray([1, 7])
 * @return {Vector} - The new vector
 */
@@ -329,10 +477,10 @@ Vector.fromArray = function(a) {
 
 /**
 * Create a vector from an object
-* // returns Vector(1, 5, 9)
+* // return Vector(1, 5, 9)
 * v = new Vector.fromArray({x: 5, y: 7, z: 9})
 * @example
-* // returns Vector(3, 0, 4)
+* // return Vector(3, 0, 4)
 * v = new Vector.fromArray({x: 3, z: 4})
 * @return {Vector} - The new vector
 */
