8b65c724e9bcca2b573670040fc7d7842cae78bb
lorossi <mail@lorenzoros.si>
2023-01-17 15:08:45 +0100

    added tests

--- tests/test.vectors.js
+++ tests/test.vectors.js
@@ -1,10 +1,11 @@
 mocha.setup("bdd");
 
-const TESTS_NUM = 100;
+const TESTS_NUM = 1000;
 const EPS = 1e-6;
 
 const magnitude_sq = (v) => v.x * v.x + v.y * v.y + v.z * v.z;
 const magnitude = (v) => Math.sqrt(magnitude_sq(v));
+const heading = (v) => Math.atan2(v.y, v.x);
 const dot = (v1, v2) => v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
 const cross = (v1, v2) => {
   const x = v1.y * v2.z - v1.z * v2.y;
@@ -13,9 +14,8 @@ const cross = (v1, v2) => {
 
   return new Vector(x, y, z);
 };
-const angle_between = (v1, v2) => {
+const angle_between = (v1, v2) =>
   Math.acos(dot(v1, v2) / (magnitude(v1) * magnitude(v2)));
-};
 const equals = (v1, v2) => {
   return (
     Math.abs(v1.x - v2.x) < EPS &&
@@ -27,6 +27,12 @@ const rotate = (v, angle) => {
   const x = v.x * Math.cos(angle) - v.y * Math.sin(angle);
   const y = v.x * Math.sin(angle) + v.y * Math.cos(angle);
   return new Vector(x, y, v.z);
+};
+const polar_to_cartesian = (r, theta, phi = 0) => {
+  const x = r * Math.sin(theta) * Math.cos(phi);
+  const y = r * Math.sin(theta) * Math.sin(phi);
+  const z = r * Math.cos(theta);
+  return { x, y, z };
 };
 
 describe("Test Vectors", () => {
@@ -51,45 +57,86 @@ describe("Test Vectors", () => {
       chai.expect(v.y).to.be.within(-1, 1);
       chai.expect(v.z).to.equal(0);
 
-      chai.expect(magnitude(v)).to.be.within(1 - EPS, 1 + EPS);
+      chai.expect(magnitude(v)).to.be.closeTo(1, EPS);
       chai.expect(v.heading2D()).to.be.within(-Math.PI, Math.PI);
 
       const w = Vector.random3D();
       chai.expect(w.x).to.be.within(-1, 1);
       chai.expect(w.y).to.be.within(-1, 1);
       chai.expect(w.z).to.be.within(-1, 1);
-      chai.expect(magnitude(w)).to.be.within(1 - EPS, 1 + EPS);
+      chai.expect(magnitude(w)).to.be.closeTo(1, EPS);
       chai.expect(() => w.heading2D()).to.throw();
     }
   });
 
-  it("should be able to create vectors from array and objects", () => {
+  it("should be able to create vectors from arrays", () => {
     const v = Vector.fromArray([1, 2]);
     chai.expect(v.x).to.equal(1);
     chai.expect(v.y).to.equal(2);
     chai.expect(v.z).to.equal(0);
+  });
 
+  it("should be able to create vectors from objects", () => {
     const w = Vector.fromObject({ x: 3, y: 4, z: 5 });
     chai.expect(w.x).to.equal(3);
     chai.expect(w.y).to.equal(4);
     chai.expect(w.z).to.equal(5);
   });
 
+  it("should be able to create vectors from a string", () => {
+    const v = Vector.fromString("Vector(1, 2, 3)");
+    chai.expect(v.x).to.equal(1);
+    chai.expect(v.y).to.equal(2);
+    chai.expect(v.z).to.equal(3);
+  });
+
   it("should be able to create vectors from 2D and 3D angles", () => {
     const v = Vector.fromAngle2D(Math.PI / 2);
-    chai.expect(v.x).to.be.within(0, EPS);
-    chai.expect(v.y).to.be.within(1 - EPS, 1 + EPS);
-    chai.expect(v.z).to.be.within(0, EPS);
+    chai.expect(v.x).to.be.closeTo(0, EPS);
+    chai.expect(v.y).to.be.closeTo(1, EPS);
+    chai.expect(v.z).to.be.closeTo(0, EPS);
+    const angle = heading(v);
+    chai.expect(angle).to.be.within(Math.PI / 2 - EPS, Math.PI / 2 + EPS);
 
     const w = Vector.fromAngle3D(Math.PI / 2, Math.PI / 2);
-    chai.expect(w.x).to.be.within(0, EPS);
-    chai.expect(w.y).to.be.within(1 - EPS, 1 + EPS);
-    chai.expect(w.z).to.be.within(0, EPS);
+    chai.expect(w.x).to.be.closeTo(0, EPS);
+    chai.expect(w.y).to.be.closeTo(1, EPS);
+    chai.expect(w.z).to.be.closeTo(0, EPS);
+  });
+
+  it("should be able to create a vector from a polar coordinate", () => {
+    const rho = 10;
+    const theta = Math.PI / 10;
+    const phi = Math.PI / 5;
+    const { x, y, z } = polar_to_cartesian(rho, theta, phi);
+    const v = Vector.fromPolar(rho, theta, phi);
+
+    chai.expect(v.x).to.be.closeTo(x, EPS);
+    chai.expect(v.y).to.be.closeTo(y, EPS);
+    chai.expect(v.z).to.be.closeTo(z, EPS);
   });
 
   it("should be able to convert a vector to a string", () => {
     const v = new Vector(1, 2, 3);
-    chai.expect(v.toString()).to.equal("Vector(x: 1, y: 2, z: 3)");
+    chai.expect(v.toString()).to.equal("Vector(1, 2, 3)");
+  });
+
+  it("should be able to convert a vector to an array", () => {
+    const v = new Vector(1, 2, 3);
+    chai.expect(v.toArray()).to.deep.equal([1, 2, 3]);
+  });
+
+  it("should be able to serialize and deserialize a vector", () => {
+    const v = new Vector(1, 2, 3);
+
+    const a = Vector.fromArray(v.toArray());
+    chai.expect(equals(v, a)).to.be.true;
+
+    const o = Vector.fromObject(v.toObject());
+    chai.expect(equals(v, o)).to.be.true;
+
+    const s = Vector.fromString(v.toString());
+    chai.expect(equals(v, s)).to.be.true;
   });
 
   it("should be able to add vectors", () => {
@@ -171,7 +218,6 @@ describe("Test Vectors", () => {
     chai.expect(() => v.div("1")).to.throw();
 
     const x = new Vector(1, 2, 3);
-    const y = new Vector(4, 5, 6);
 
     x.multiply(w);
     chai.expect(x.x).to.equal(4);
@@ -211,6 +257,13 @@ describe("Test Vectors", () => {
     chai.expect(() => v.cross("1")).to.throw();
   });
 
+  it("should calculate the heading of a vector", () => {
+    const v = new Vector(1, 2);
+    chai.expect(v.heading2D()).to.equal(heading(v));
+  });
+
+  it("should create the vector from an angle", () => {});
+
   it("should calculate the distance of a vector", () => {
     const v = new Vector(1, 2, 3);
     const u = new Vector(4, 5, 6);
@@ -238,11 +291,11 @@ describe("Test Vectors", () => {
     const v = new Vector(1, 2, 3);
     const u = new Vector(4, 5, 6);
 
-    chai.expect(v.equals(u)).to.equal(false);
-    chai.expect(u.equals(v)).to.equal(false);
+    chai.expect(v.equals(u)).to.be.false;
+    chai.expect(u.equals(v)).to.be.false;
 
-    chai.expect(v.equals(v)).to.equal(true);
-    chai.expect(u.equals(u)).to.equal(true);
+    chai.expect(v.equals(v)).to.be.true;
+    chai.expect(u.equals(u)).to.be.true;
 
     chai.expect(() => v.equals("1")).to.throw();
     chai.expect(() => v.equals()).to.throw();
@@ -252,13 +305,13 @@ describe("Test Vectors", () => {
     const v = new Vector(1, 2, 3);
     const u = v.copy();
 
-    chai.expect(equals(v, u)).to.equal(true);
-    chai.expect(v.equals(u)).to.equal(true);
-    chai.expect(u.equals(v)).to.equal(true);
+    chai.expect(equals(v, u)).to.be.true;
+    chai.expect(v.equals(u)).to.be.true;
+    chai.expect(u.equals(v)).to.be.true;
 
     const z = v.copy();
     z.z = 4;
-    chai.expect(z.equals(v)).to.equal(false);
+    chai.expect(z.equals(v)).to.be.false;
   });
 
   it("should limit a vector", () => {
@@ -279,7 +332,7 @@ describe("Test Vectors", () => {
     const v = new Vector(1, 2, 3);
     const u = rotate(v, 2);
     v.rotate(2);
-    chai.expect(equals(v, u)).to.equal(true);
+    chai.expect(equals(v, u)).to.be.true;
     chai.expect(() => v.rotate("1")).to.throw();
   });
 
@@ -293,7 +346,6 @@ describe("Test Vectors", () => {
     for (const x of [true, false]) {
       for (const y of [true, false]) {
         for (const z of [true, false]) {
-          console.log({ x, y, z });
           const v = new Vector(1, 2, 3);
           v.invert(x, y, z);
           chai.expect(v.x).to.equal(x ? -1 : 1);
@@ -323,5 +375,90 @@ describe("Test Vectors", () => {
     const v = new Vector(1, 2, 3);
     chai.expect(v.mag()).to.equal(magnitude(v));
     chai.expect(v.magSq()).to.equal(magnitude_sq(v));
+  });
+
+  it("should have working setters and getters", () => {
+    const v = new Vector(1, 2, 3);
+    chai.expect(v.x).to.equal(1);
+    chai.expect(v.y).to.equal(2);
+    chai.expect(v.z).to.equal(3);
+
+    v.x = 4;
+    v.y = 5;
+    v.z = 6;
+
+    chai.expect(v.x).to.equal(4);
+    chai.expect(v.y).to.equal(5);
+    chai.expect(v.z).to.equal(6);
+  });
+});
+
+describe("Random Vector Tests", () => {
+  it("the magnitude of a random vector should be 1", () => {
+    for (let i = 0; i < TESTS_NUM; i++) {
+      const v = Vector.random2D();
+      const u = Vector.random3D();
+
+      chai.expect(v.mag()).to.be.closeTo(1, EPS);
+      chai.expect(u.mag()).to.be.closeTo(1, EPS);
+    }
+  });
+
+  it("rotating a vector should not change its magnitude", () => {
+    for (let i = 0; i < TESTS_NUM; i++) {
+      const m = Math.random() * 100;
+
+      const v = Vector.random2D();
+      const u = Vector.random3D();
+
+      v.setMag(m);
+      u.setMag(m);
+      v.rotate(Math.random() * 2 * Math.PI);
+      u.rotate(Math.random() * 2 * Math.PI);
+
+      chai.expect(v.mag()).to.be.closeTo(m, EPS);
+      chai.expect(u.mag()).to.be.closeTo(m, EPS);
+    }
+  });
+
+  it("testing dot and cross products of random vectors", () => {
+    for (let i = 0; i < TESTS_NUM; i++) {
+      const v = Vector.random2D();
+      const u = Vector.random3D();
+
+      const d = dot(v, u);
+      const c = cross(v, u);
+
+      chai.expect(v.copy().dot(u)).to.be.closeTo(d, EPS);
+      chai.expect(u.copy().dot(v)).to.be.closeTo(d, EPS);
+
+      chai.expect(v.copy().cross(u)).to.deep.equal(c);
+      chai.expect(u.copy().cross(v)).to.deep.equal(c.invert());
+    }
+  });
+
+  it("testing angle between random vectors", () => {
+    for (let i = 0; i < TESTS_NUM; i++) {
+      const v = Vector.random2D();
+      const u = Vector.random3D();
+
+      const a = angle_between(v, u);
+
+      chai.expect(v.angleBetween(u)).to.be.closeTo(a, EPS);
+      chai.expect(u.angleBetween(v)).to.be.closeTo(a, EPS);
+    }
+  });
+
+  it("testing vector serialization and deserialization", () => {
+    for (let i = 0; i < TESTS_NUM; i++) {
+      const v = Vector.random3D();
+
+      const a = Vector.fromArray(v.toArray());
+      chai.expect(equals(v, a)).to.be.true;
+      const o = Vector.fromObject(v.toObject());
+      chai.expect(equals(v, o)).to.be.true;
+      const s = Vector.fromString(v.toString());
+      chai.expect(equals(v, s)).to.be.true;
+    }
   });
 });
